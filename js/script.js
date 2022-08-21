8e3f4368aee747383b7a3cf722fd11db3b491139
Khash <khashayar.m.003@gmail.com>
2022-08-21 07:02:19 +0430

    qoutes done

---
+++ js/script.js
@@ -0,0 +1,74 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+    quote:"Don't Aspire To Make A Living , Aspire To Make A Difference",
+    source: "Denzel Washington",
+    Citation: "Speech At Dillard University",
+    year: 2016
+  },
+  {
+    quote:"Stay Hungry, Stay Foolish",
+    source: "Steve Jobs",
+    Citation: "speech at Stanford University ",
+    year: 2005
+  },
+  {
+    quote:"You Cannot Change The Past, But You Can Change Your Perspective About It ",
+    source: "Marcus Taylor",
+    Citation: "Motivational Video - Perspective",
+    year: 2021
+  },
+  {
+    quote:"If an egg is broken by an outside force life ends . if broken by an inside force life begins , great things always begin on the inside.",
+    source: "Jim Kwik",
+    Citation: "Anonymous",
+    year: 2018
+  },
+  {
+    quote:"As is a tale, so is life: not how long it is, but how good it is, is what matters.",
+    source: "J.K Rowling",
+    Citation: "Harward Speech",
+    year: 2011
+  },
+  {
+    quote:"Have More Than You Show, Speak Less Than You Know",
+    source: "William Shakespear",
+    Citation: "King Lear",
+    year: 1606
+  },
+  {
+    quote:"The Tree That Would Grow To Heave, Must Send Its Roots To Hell",
+    source: "Carl Jung",
+    Citation: "chapter 5 of a book by Jung",
+    year: 1951
+  }
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
